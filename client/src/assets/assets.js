5c4fcbfa17cd8a52c9c5e6b9c069dea932918fe6
aldensd-1211 <aldendcunha1211@gmail.com>
2024-11-01 17:50:50 +0530

    added logo

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,10 +2,14 @@ import google_logo from "./google_logo.png";
 import login_cover from "./login_cover.png";
 import profile_pic from "./profile_pic.png";
 import mask from "./mask.png";
+import logo from "./logo.png";
+import logo2 from "./logo2.png";
 
 export const assets = {
   google_logo,
   login_cover,
   profile_pic,
   mask,
+  logo,
+  logo2,
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,39 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../redux/authSlice";
+import { assets } from "../assets/assets";
 
 const Header = () => {
-  const isAuthenticated= useSelector((state)=>state.auth.isAuthenticated);
-  const navigate=useNavigate();
-  const dispatch=useDispatch();
-  const handleauth=()=>{
-    if(!isAuthenticated)navigate('/auth');
-    else{
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleauth = () => {
+    if (!isAuthenticated) navigate("/auth");
+    else {
       dispatch(logout());
     }
-  }
+  };
 
   return (
     <header className="fixed w-full z-50 flex justify-around items-center p-2 gap-80 bg-[#ffffff] shadow-lg">
       <div className="text-3xl font-bold">
-        <a href="/" className=" no-underline hover:underline">Kreativ</a>
+        <a href="/" className=" no-underline hover:underline">
+          <img className=" w-40" src={assets.logo2} alt="" />
+        </a>
       </div>
       <nav className="flex gap-10 justify-center text-base ">
-        <a href="/blog" className=" no-underline flex items-center  hover:text-main">Blogs</a>
-        <button onClick={handleauth} className="bg-main text-white items-center py-2 px-3  rounded  no-underline hover:bg-purple-800">{isAuthenticated?"Logout":"Login"  }</button>
+        <a
+          href="/blog"
+          className=" no-underline flex items-center  hover:text-main"
+        >
+          Blogs
+        </a>
+        <button
+          onClick={handleauth}
+          className="bg-main text-white items-center py-2 px-3  rounded  no-underline hover:bg-purple-800"
+        >
+          {isAuthenticated ? "Logout" : "Login"}
+        </button>
       </nav>
     </header>
   );
