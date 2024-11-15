afb23ad7ad73d21a349154daa625c528e4ecef1e
Anish-H-G <anishhg02@gmail.com>
2024-11-15 15:23:32 +0530

    Resolved issues in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom"; 
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginSignup from "./pages/LoginSignup";
 import Header from "./components/Header";
@@ -9,11 +9,7 @@ import { useEffect, useState } from "react";
 import { login } from "./redux/authSlice";
 import Protected from "./constants/Protected";
 import Blogs from "./pages/Blogs";
-<<<<<<< HEAD
-import SingleBlog from "./pages/SingleBlog";  // Import SingleBlog component
-=======
 import Blog from "./pages/Blog";
->>>>>>> 2008f7ea4266995709472a1e6493d920e1263aa3
 
 function App() {
   const location = useLocation();
@@ -26,26 +22,6 @@ function App() {
     <div className="flex flex-col">
       {!hideHeader && <Header />} 
       <div className={`${!hideHeader && 'pt-14' }`}>
-<<<<<<< HEAD
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<LoginSignup />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blog/:id" element={<SingleBlog />} /> {/* Add route for SingleBlog */}
-          {isAuthChecked ? (
-            <Route
-              path="/chat"
-              element={
-                <Protected>
-                  <Chatbot />
-                </Protected>
-              }
-            />
-          ) : (
-            <Route path="/chat" element={<p>Loading...</p>} />
-          )}
-        </Routes>
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<LoginSignup />} />
@@ -64,7 +40,6 @@ function App() {
           <Route path="/chat" element={<p>Loading...</p>} />
         )}
       </Routes>
->>>>>>> 2008f7ea4266995709472a1e6493d920e1263aa3
       </div>  
       {!hideFooter && <Footer />} 
     </div>
