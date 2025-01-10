004eec81a3b3e5ef39bcf59a44e26bfed430c52c
aldensd-1211 <aldendcunha1211@gmail.com>
2025-01-10 15:49:50 +0530

    minor changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -13,5 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")],
+  plugins: [import("tailwind-scrollbar")],
 };
