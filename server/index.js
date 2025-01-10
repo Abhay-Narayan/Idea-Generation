6c5178dc87e902884622ae5c30335af5e39cb012
aldensd-1211 <aldendcunha1211@gmail.com>
2025-01-10 16:25:03 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -2,25 +2,33 @@ import express from "express";
 import mongoose from "mongoose";
 import connect from "./db.js";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 import authController from "./controllers/authController.js";
 import blogController from "./controllers/blogController.js";
 import chatController from "./controllers/chatController.js";
-import userController from "./controllers/userController.js"
+import userController from "./controllers/userController.js";
 
 dotenv.config();
 mongoose.set("strictQuery", false);
 connect();
 
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://kreativweb.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 app.use("/auth", authController);
-app.use("/blog",blogController);
-app.use("/bot",chatController);
+app.use("/blog", blogController);
+app.use("/bot", chatController);
 app.use("/user", userController);
 
 const PORT = process.env.PORT || 5000;
